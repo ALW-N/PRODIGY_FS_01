979639e6a128d857747d2d9d746962f2d98ef8f2
ALW-N <officialalwintomy@gmail.com>
2024-09-27 00:14:53 +0530

    Commit - 3

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,28 @@
+import {connect} from '@/dbConfig/dbConfig';
+import { NextRequest, NextResponse} from 'next/server';
+
+
+connect()
+
+export async function GET(request:NextRequest){
+    try {
+        const response = NextResponse.json({
+            message:"Logout Success",
+            success: true
+        })
+
+        response.cookies.set('token', '', {
+            httpOnly:true,
+            expires: new Date(0)
+        } )
+
+        return response
+        
+    }catch (error: unknown) {
+        let errorMessage = 'An unknown error occurred';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return NextResponse.json({error: errorMessage}, {status: 500})
+    }
+}
